849d82f67e0acea3bcf914941ac85a562f280043
Dewale <walekoya125@gmail.com>
2015-11-18 22:07:11 -0800

    comment style changed to work with doxygen

--- Actor.h
+++ Actor.h
@@ -9,14 +9,14 @@ public:
 	Actor() {} //Constructor 
 	~Actor() {} //Deconstructor
 
-	/*
+	/**
 	* Draws a new frame given an sfml window object.
 	*
 	* @param window - a reference to a window that serves as a target for 2D drawing.
 	*/
 	virtual void Draw(sf::RenderWindow &window) = 0;
 
-	/*
+	/**
 	* Sets the actor's position in the window.
 	* 
 	* @param xPosition - a constant reference to a floating point value which represents
@@ -26,7 +26,7 @@ public:
 	*/
 	virtual void SetPosition(const float &xPosition, const float &yPosition) { _position = sf::Vector2f(xPosition, yPosition); };
 
-	/*
+	/**
 	* Returns the actor's dimensions.
 	* 
 	* @return a 2D SFML vector describing the actor's dimensions
