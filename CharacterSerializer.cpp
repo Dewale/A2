a5a1e8eee51600d8d8243f9be39772e828b37396
Dewale <walekoya125@gmail.com>
2015-12-12 18:06:32 -0800

    Updated serializer to save and load characters

--- CharacterSerializer.cpp
+++ CharacterSerializer.cpp
@@ -28,6 +28,30 @@ void CharacterSerializer::SaveCharacter(const Character &data){
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
 
 	proto::Character character;
+	
+	character.set_level(data.GetLevel());
+	character.set_name(data.GetName());
+	character.set_xp(data.GetXp());
+	switch(data.GetSkill().GetSkillType()){
+		case SkillType::Dragon:
+		avatar.set_type(proto::Character::SkillType::Avatar_SkillType_Dragon);
+		break;
+	case SkillType::Babarian:
+		avatar.set_type(proto::Character::SkillType::Avatar_SkillType_BABARIAN);
+		break;
+	case SkillType::Archer:
+		avatar.set_type(proto::Character::SkillType::Avatar_SkillType_ARCHER);
+		break;
+	default:
+		break;
+	}
+	
+	//Writing the character back to the disk.
+	std::fstream output(data.GetName() + ".dat", std::ios::out | std::ios::trunc | std::ios::binary);
+	if (!character.SerializeToOstream(&output)){
+		std::cerr << "Failed to write cache to disk." << std::endl;
+		return;
+	}
 }
 
 
@@ -39,9 +63,32 @@ bool CharacterSerializer::LoadCharacter(const std::string &CharacterName){
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
 
 	proto::Character character;
-
+	//Loading a character
+	std::fstream input(charcterName + ".dat", std::ios::in | std::ios::binary);
+	if (!input){
+		return false;
+	}
+	else if (!character.ParseFromIstream(&input)){
+		std::cerr << "Failed to parse file." << std::endl;
+		return false;
+	}
+	SkillType type;
+	switch (character.type()){
+	case proto::Character::SkillType::Character_SkillType_DRAGON:
+		type = SkillType::Dragon;
+		break;
+	case proto::Character::SkillType::Character_SkillType_BABARIAN:
+		type = SkillType::Babarian;
+		break;
+	case proto::Character::SkillType::Character_SkillType_ARCHER:
+		type = SkillType::Archer;
+		break;
+	default:
+		break;
+	}
+	
 	if (_currentPlayer) delete _currentPlayer;
-	_currentPlayer = new Character(CharacterName, 1, CharacterClass::GetClassType(CharacterName));
+	_currentPlayer = new Character(character.name(), character.level(), type, character.xp());
 	return true;
 }
 
